3cb1e94e1a9d7838146a60da27c4dd4266183c51
jcpersin <jcpersin@ncsu.edu>
2016-07-25 19:23:31 -0400

    changing title picture

--- src/header/title.js
+++ src/header/title.js
@@ -5,6 +5,6 @@ export default class Title extends React.Component {
         super(props);
     }
     render() {
-        return <img className={styles.title_img} src="http://assets.worldwildlife.org/photos/2325/images/hero_full/mountains-hero.jpg?1345838509" /> //http://constantine.typepad.com/.a/6a0120a7fc3be9970b01b7c7d26604970b-pi
+        return <img className={styles.title_img} src="http://www.quickanddirtytips.com/sites/default/files/images/4448/Dog_Chew.jpg" /> //http://constantine.typepad.com/.a/6a0120a7fc3be9970b01b7c7d26604970b-pi
     }
 }
